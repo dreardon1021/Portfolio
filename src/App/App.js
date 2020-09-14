235b109579dc4ed68bc45d5e5aa1132e103f6d59
Dan Reardon <dreardon1021@gmail.com>
2020-09-14 15:44:00 -0600

    Setup React Router and adjust href in anchors

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../Header/Header.js';
 import Footer from '../Footer/Footer.js'
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
   return (
     <body>
       <Header />
+      <Switch>
+
+      </Switch>
       <Footer />
     </body>
   );
--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -7,15 +7,16 @@ const Footer = () => {
       <hr className="white-line" />
       <div className="nav-bar-area">
       <nav>
+        <a className="nav-bar-item" href="/">Home</a>
         <a className="nav-bar-item" href="/resume">Resume</a>
         <a className="nav-bar-item" href="/projects">Projects</a>
-        <a className="nav-bar-item" href="https://www.linkedin.com/in/danreardon21/">Linkedin</a>
-        <a className="nav-bar-item" href="https://github.com/dreardon1021">Github</a>
-        <a className="nav-bar-item" href="https://alumni.turing.io/alumni/dan-reardon">Alumni Profile</a>
+        <a className="nav-bar-item" target="_blank" href="https://www.linkedin.com/in/danreardon21/">Linkedin</a>
+        <a className="nav-bar-item" target="_blank" href="https://github.com/dreardon1021">Github</a>
+        <a className="nav-bar-item" target="_blank" href="https://alumni.turing.io/alumni/dan-reardon">Alumni Profile</a>
       </nav>
       <div className="contact-container">
-        <p className="white-text contact-text">Email: dreardon1021@gmail.com</p>
-        <p className="white-text contact-text">Phone: 973-494-1040</p>
+        <p className="contact-text">Email: dreardon1021@gmail.com</p>
+        <p className="contact-text">Phone: 973-494-1040</p>
       </div>
       </div>
     </footer>
