ca98e70ba0d6068f8bb8e03f0b1789e9ce2c7b8a
Dan Reardon <dreardon1021@gmail.com>
2020-09-14 15:32:14 -0600

    Create Footer component and add nav

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header/Header.js';
+import Footer from '../Footer/Footer.js'
 import './App.css';
 
 const App = () => {
   return (
     <body>
       <Header />
+      <Footer />
     </body>
   );
 }
--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <hr className="white-line" />
-    <div className="contact-container">
-      <p className="white-text contact-text">Email: dreardon1021@gmail.com</p>
-      <p className="white-text contact-text">Phone: 973-494-1040</p>
-    </div>
-
+    <footer>
+      <hr className="white-line" />
+      <div className="nav-bar-area">
+      <nav>
+        <a className="nav-bar-item" href="/resume">Resume</a>
+        <a className="nav-bar-item" href="/projects">Projects</a>
+        <a className="nav-bar-item" href="https://www.linkedin.com/in/danreardon21/">Linkedin</a>
+        <a className="nav-bar-item" href="https://github.com/dreardon1021">Github</a>
+        <a className="nav-bar-item" href="https://alumni.turing.io/alumni/dan-reardon">Alumni Profile</a>
+      </nav>
+      <div className="contact-container">
+        <p className="white-text contact-text">Email: dreardon1021@gmail.com</p>
+        <p className="white-text contact-text">Phone: 973-494-1040</p>
+      </div>
+      </div>
+    </footer>
   )
 }
+
+export default Footer
