717d438682b9ae19c8033dc6aa2478ea408ae636
Dan Reardon <dreardon1021@gmail.com>
2020-09-14 11:18:59 -0600

    Create Header component with logo

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+import '../App/App.css'
+
+const Footer = () => {
+  return (
+    <hr className="white-line" />
+    <div className="contact-container">
+        <p className="white-text contact-text">Email: dreardon1021@gmail.com</p>
+        <p className="white-text contact-text">Phone: 973-494-1040</p>
+    </div>
+
+  )
+}
--- src/Header/header.js
+++ src/Header/header.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import logo from '../images/Logo.png';
-import './header.css'
+import React from 'react' 
+import './Header.css'
+import '../index.css'
 
 const Header = () => {
   return (
     <header>
       <div className="logo-container">
-        <img id="header-image" src={logo} alt="logo"/>
-        <p id="header-text"></p>
-      </div>
-      <div className="contact-container">
-        <p className="contact-text">Email: dreardon1021@gmail.com</p>
-        <p className="contact-text">Phone: 973-494-1040</p>
+        <div className="DR-logo">
+          <p className="logo-D header-logo-text">D</p>
+          <p className="logo-R header-logo-text">R</p>
+        </div>
+        <div className="header-name-title-container">
+          <p className="header-name-title">Dan Reardon</p>
+          <p className="header-name-title">Software Engineer</p>
+        </div>
       </div>
     </header>
   )
