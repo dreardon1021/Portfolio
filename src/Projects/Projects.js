ccb49c907f9ea8f37211282f8986377c2f745fef
Dan Reardon <dreardon1021@gmail.com>
2020-09-15 16:51:59 -0600

    Move images folder to public and display projects

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -5,7 +5,16 @@ import projectsList from '../projects-list'
 const Projects = () => {
   return (
     <section className="projects-container">
-
+      {projectsList.map(project => {
+        return(
+          <article key={project.id} className="project-card">
+            <h2 className="white-text">{project.name}</h2>
+            <img className="project-card-image" src={project.image} alt={project.name} />
+            <a href={project.id}><button>View</button></a>
+            {console.log(project.image)}
+          </article>
+        )
+      })}
     </section>
   )
 }
--- src/projects-list.js
+++ src/projects-list.js
@@ -1,5 +1,6 @@
-const projectList = [
+const projectsList = [
   {
+    id: 1,
     name: 'Vacation Rentals Around Denver',
     description: 'This was a paired project where Dan Reardon and Ed Stoner created a housing rentals application for the denver area. The main focus point for this project was React. We used React, React Router, JSX, CSS, React-Testing-Library, Jest and Leaflet for this project.',
     login: true,
@@ -11,6 +12,7 @@ const projectList = [
     workInProgress: false
   },
   {
+    id: 2,
     name: 'Covents',
     description: 'COVENTS is an app providing easy access to virtual events happening around the globe in the era of COVID-19. Our goal is to help preserve a sense of togetherness while social distancing through live entertainment. Upon opening the app, the user is able to view upcoming events gathered by a webscraper and compiled into a single API. The user can favorite events as they browse. Favorited events populate in a section of the nav bar on the left so the user has easy access to the event name, date, and a link to the external details page for that event. The user is also able to filter events by clicking a date on the calendar widget or by entering a search term in the search bar. If there are no matching events, an alert will pop up and the page will redirect back to the home display.',
     login: false,
@@ -22,6 +24,7 @@ const projectList = [
     workInProgress: true
   },
   {
+    id: 3,
     name: 'Rancid-Tomatillos',
     description: 'Rancid Tomatillos is an application that allows users to browse movies, log into the server, rate movies, and view additional details about each movie. It is a showcase of our teams ability to work with progressively more complex React component structures. We used redux as a global store to minimize prop drilling and practice data management for large scale applications. We use React Router to navigate between pages and testes all of our synchronous and async functionality.',
     login: true,
@@ -33,6 +36,7 @@ const projectList = [
     workInProgress: false
   },
   {
+    id: 4,
     name: 'Hearthstone For Beginners',
     description: 'This is an application that allows the user to create card lists for the computer card game known as Hearthstone. The user can save these card lists and revisit them when they want to. You can also get a detailed breakdown of each card when you view it. The user can view it by clicking view below the card or by clicking the card name in their deck list. To remove cards you navigate to "View Current Deck" and click remove. To Add, simply click whereever you see an add button available to you.',
     login: false,
@@ -45,4 +49,4 @@ const projectList = [
   }
 ]
 
-export default projectList
+export default projectsList
