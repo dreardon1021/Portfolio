81b9d8d97c2af1684efd4f866e1d3cdcc123b107
Dan Reardon <dreardon1021@gmail.com>
2020-09-15 16:06:15 -0600

    Setup Project component

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Projects.css'
+import projectsList from '../projects-list'
+
+const Projects = () => {
+  return (
+    <section className="projects-container">
+
+    </section>
+  )
+}
+
+export default Projects
