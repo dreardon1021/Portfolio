cbbc829074684fb52fd5b052e69fc1ab10f26c1d
Dan Reardon <dreardon1021@gmail.com>
2020-09-21 16:58:25 -0600

    Adjust spacing

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -7,18 +7,18 @@ const Footer = () => {
     <footer>
       <hr className="white-line" />
       <div className="nav-bar-area">
-      <nav>
-        <NavLink className="nav-bar-item" to="/">Home</NavLink>
-        <NavLink className="nav-bar-item" to="/resume">Resume</NavLink>
-        <NavLink className="nav-bar-item" to="/projects">Projects</NavLink>
-        <a className="nav-bar-item" target="_blank" href="https://www.linkedin.com/in/danreardon21/">Linkedin</a>
-        <a className="nav-bar-item" target="_blank" href="https://github.com/dreardon1021">Github</a>
-        <a className="nav-bar-item" target="_blank" href="https://alumni.turing.io/alumni/dan-reardon">Alumni Profile</a>
-      </nav>
-      <div className="contact-container">
-        <p className="contact-text">Email: dreardon1021@gmail.com</p>
-        <p className="contact-text">Phone: 973-494-1040</p>
-      </div>
+        <nav>
+          <NavLink className="nav-bar-item" to="/">Home</NavLink>
+          <NavLink className="nav-bar-item" to="/resume">Resume</NavLink>
+          <NavLink className="nav-bar-item" to="/projects">Projects</NavLink>
+          <a className="nav-bar-item" target="_blank" href="https://www.linkedin.com/in/danreardon21/">Linkedin</a>
+          <a className="nav-bar-item" target="_blank" href="https://github.com/dreardon1021">Github</a>
+          <a className="nav-bar-item" target="_blank" href="https://alumni.turing.io/alumni/dan-reardon">Alumni Profile</a>
+        </nav>
+        <div className="contact-container">
+          <p className="contact-text">Email: dreardon1021@gmail.com</p>
+          <p className="contact-text">Phone: 973-494-1040</p>
+        </div>
       </div>
     </footer>
   )
