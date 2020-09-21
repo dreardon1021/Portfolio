de6da3f52cf1b192116ea8efd07d16364cb67305
Dan Reardon <dreardon1021@gmail.com>
2020-09-21 15:01:59 -0600

    Updates

---
+++ precache-manifest.2ccf7ad4caff4c3b933852c2a6b5e13e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c261f46b91e99488c8bc043b507471",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "87f120ce3847f1e00399",
+    "url": "/Portfolio/static/css/main.498b218d.chunk.css"
+  },
+  {
+    "revision": "585be270f1bedab54407",
+    "url": "/Portfolio/static/js/2.99388e5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.99388e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f120ce3847f1e00399",
+    "url": "/Portfolio/static/js/main.45cc3d7e.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "b17a9eef998612d0bddfd11d61dd048a",
+    "url": "/Portfolio/static/media/Background-Portfolio-1.b17a9eef.jpg"
+  },
+  {
+    "revision": "96ef646adaafc63b4638cb021c614016",
+    "url": "/Portfolio/static/media/Dan.96ef646a.jpeg"
+  },
+  {
+    "revision": "40f95782fd8676bffe6bba99824fcb8e",
+    "url": "/Portfolio/static/media/resume.40f95782.pdf"
+  }
+]);
