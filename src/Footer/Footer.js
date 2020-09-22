86d0e58ac8a1cbeb22218fa506d7d712c01a4a58
Dan Reardon <dreardon1021@gmail.com>
2020-09-22 16:10:56 -0600

    Add target"_blank" to footer for proper routing

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -11,9 +11,9 @@ const Footer = () => {
           <NavLink className="nav-bar-item" to="/">Home</NavLink>
           <NavLink className="nav-bar-item" to="/resume">Resume</NavLink>
           <NavLink className="nav-bar-item" to="/projects">Projects</NavLink>
-          <a className="nav-bar-item" href="https://www.linkedin.com/in/danreardon21/">Linkedin</a>
-          <a className="nav-bar-item" href="https://github.com/dreardon1021">Github</a>
-          <a className="nav-bar-item" href="https://alumni.turing.io/alumni/dan-reardon">Alumni Profile</a>
+          <a target="_blank" rel="noopener noreferrer" className="nav-bar-item" href="https://www.linkedin.com/in/danreardon21/">Linkedin</a>
+          <a target="_blank" rel="noopener noreferrer" className="nav-bar-item" href="https://github.com/dreardon1021">Github</a>
+          <a target="_blank" rel="noopener noreferrer" className="nav-bar-item" href="https://alumni.turing.io/alumni/dan-reardon">Alumni Profile</a>
         </nav>
         <div className="contact-container">
           <p className="contact-text">Email: dreardon1021@gmail.com</p>
