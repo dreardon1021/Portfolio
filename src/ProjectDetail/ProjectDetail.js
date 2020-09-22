5cb361e4f2cd88a7dccb4433a37dd22bb1a9d576
Dan Reardon <dreardon1021@gmail.com>
2020-09-21 22:07:39 -0600

    Add general styling for ProjectDetail

--- src/ProjectDetail/ProjectDetail.js
+++ src/ProjectDetail/ProjectDetail.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import projectList from '../projects-list'
-import './ProjectDetail.css'
-import '../App/App.css'
+import React from 'react';
+import projectList from '../projects-list';
+import './ProjectDetail.css';
+import '../App/App.css';
 
 const ProjectDetail = props => {
-  let project = projectList.find(project => project.id === props.id)
+  let project = projectList.find(project => project.id === props.id);
 
   return(
     <section className="project-detail-container">
       <h2 className="white-text">{project.name}</h2>
       <div className="project-img-description-container">
-        <img classname="" src={project.image} alt={project.name}></img>
-        <p className="white-text">{project.description}</p>
+        <img className="project-detail-image" src={project.image} alt={project.name}></img>
+        <p className="white-text project-detail-description">{project.description}</p>
       </div>
       <div className="detail-buttons-container">
         <button className="details-nav-button">Github Repository</button>
