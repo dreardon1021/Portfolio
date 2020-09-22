0b00704e5f3a76782ebd6ff393af8a13b4b9e46f
Dan Reardon <dreardon1021@gmail.com>
2020-09-22 14:59:58 -0600

    Quick grammar fix

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -6,8 +6,8 @@ import headshot from '../images/Dan.jpeg'
 const Homepage = () => {
   return (
     <div className="home-content-container">
-      <p className="white-text summary">Hello! My name is Dan Reardon and  I am a Denver based Software Engineer. I have extensive experience leading
-        teams in B2B partnerships and assessing client needs from my past as a restaurant manager.  I am passionate
+      <p className="white-text summary">Hello! My name is Dan Reardon and I am a Denver based Software Engineer. I have extensive experience leading
+        teams in B2B partnerships and assessing client needs from my past management experience.  I am passionate
         about creating front-end and user experiences. Currently seeking a Software Engineer/Developer position with a
         company to produce high-quality solutions for customers, bringing programming experience in Javascript,
         JavaScript frameworks, and Python utilizing Test-Driven Development.</p>
