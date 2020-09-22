0c767b3be71a13c0943916644bc4af3879ec6d5a
Dan Reardon <dreardon1021@gmail.com>
2020-09-22 14:59:50 -0600

    Switch from functional to class component

--- src/ProjectDetail/ProjectDetail.js
+++ src/ProjectDetail/ProjectDetail.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import projectList from '../projects-list';
 import './ProjectDetail.css';
 import '../App/App.css';
 
-const ProjectDetail = props => {
-  let project = projectList.find(project => project.id === props.id);
 
-  return(
-    <section className="project-detail-container">
-      <h2 className="white-text">{project.name}</h2>
-      <div className="project-img-description-container">
-        <img className="project-detail-image" src={project.image} alt={project.name}></img>
-        <div className="project-description-container">
-          <p className="white-text project-detail-description">{project.description}</p>
+class ProjectDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      project: projectList.find(project => project.id === props.id)
+    }
+  }
+
+  render() {
+    return(
+      <section className="project-detail-container">
+        <h2 className="white-text">{this.state.project.name}</h2>
+        <div className="project-img-description-container">
+          <img className="project-detail-image" src={this.state.project.image} alt={this.state.project.name}></img>
+          <div className="project-description-container">
+            <p className="white-text project-detail-description">{this.state.project.description}</p>
+          </div>
         </div>
-      </div>
-      <div className="detail-buttons-container">
-        <a href={project.githubLink} className="text-decoration-none details-anchor"><button className="details-nav-button">Github Repository</button></a>
-        <a href={project.deployedLink} className="text-decoration-none details-anchor"><button className="details-nav-button">Deployed Link</button></a>
-      </div>
-    </section>
-  )
+        <div className="detail-buttons-container">
+          <a href={this.state.project.githubLink} className="text-decoration-none details-anchor"><button className="details-nav-button">Github Repository</button></a>
+          <a href={this.state.project.deployedLink} className="text-decoration-none details-anchor"><button className="details-nav-button">Deployed Link</button></a>
+        </div>
+      </section>
+    )
+  }
+  
 }
 
 export default ProjectDetail
