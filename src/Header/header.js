eabb0cbc791f9b7d158ee2311a8ed79664dd297e
Dan Reardon <dreardon1021@gmail.com>
2020-09-23 21:36:37 -0600

    Fix Header logo from <a> to <NavLink>

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import './Header.css'
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
-      <a className="text-decoration-none" href="/">
-      <div className="logo-container">
-        <div className="DR-logo">
-          <p className="logo-D header-logo-text">D</p>
-          <p className="logo-R header-logo-text">R</p>
+      <NavLink className="text-decoration-none" to="/">
+        <div className="logo-container">
+          <div className="DR-logo">
+            <p className="logo-D header-logo-text">D</p>
+            <p className="logo-R header-logo-text">R</p>
+          </div>
+          <div className="header-name-title-container">
+            <p className="header-name-title">Dan Reardon</p>
+            <p className="header-name-title">Software Engineer</p>
+          </div>
         </div>
-        <div className="header-name-title-container">
-          <p className="header-name-title">Dan Reardon</p>
-          <p className="header-name-title">Software Engineer</p>
-        </div>
-      </div>
-      </a>
+      </NavLink>
     </header>
   )
 }
