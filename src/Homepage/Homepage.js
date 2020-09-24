989cb7ff273f4928e28a9af594d5df615be0054c
Dan Reardon <dreardon1021@gmail.com>
2020-09-24 13:20:51 -0600

    Grammer fix homepage

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -6,11 +6,11 @@ import headshot from '../images/Dan.jpeg'
 const Homepage = () => {
   return (
     <div className="home-content-container">
-      <p className="white-text summary">Hello!  My name is Dan Reardon, a Denver based
-      Software Engineer.  I have extensive experience leading teams in B2B partnerships and
+      <p className="white-text summary">Hello! My name is Dan Reardon, a Denver based
+      Software Engineer. I have extensive experience leading teams in B2B partnerships and
       assessing client needs from past management positions. I am passionate about creating
       dynamic front-end user experiences and currently seeking a Software Engineer/Developer
-      position with an organization focused on client solutions. My Programming experience
+      position with an organization focused on client solutions. My programming experience
       includes JavaScript, React, Node, and Python, utilizing Test-Driven Development.</p>
       <img className="headshot" src={headshot} alt="headshot"/>
     </div>
