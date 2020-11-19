e6e0fd5d7fbe13c4cb218d9b405aab491f5ddb03
Dan Reardon <dreardon1021@gmail.com>
2020-11-19 16:56:59 -0700

    Add new resume

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import './Footer.css'
-import '../App/App.css'
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import "./Footer.css";
+import "../App/App.css";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -9,12 +9,39 @@ const Footer = () => {
       <hr className="white-line" />
       <div className="nav-bar-area">
         <nav>
-          <NavLink className="nav-bar-item" to="/">Home</NavLink>
-          <NavLink className="nav-bar-item" to="/resume">Resume</NavLink>
-          <NavLink className="nav-bar-item" to="/projects">Projects</NavLink>
-          <a target="_blank" rel="noopener noreferrer" className="nav-bar-item" href="https://www.linkedin.com/in/danreardon21/">Linkedin</a>
-          <a target="_blank" rel="noopener noreferrer" className="nav-bar-item" href="https://github.com/dreardon1021">Github</a>
-          <a target="_blank" rel="noopener noreferrer" className="nav-bar-item" href="https://alumni.turing.io/alumni/dan-reardon">Alumni Profile</a>
+          <NavLink className="nav-bar-item" to="/">
+            Home
+          </NavLink>
+          <NavLink className="nav-bar-item" to="/resume">
+            Resume
+          </NavLink>
+          <NavLink className="nav-bar-item" to="/projects">
+            Projects
+          </NavLink>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="nav-bar-item"
+            href="https://www.linkedin.com/in/danreardon21/"
+          >
+            Linkedin
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="nav-bar-item"
+            href="https://github.com/dreardon1021"
+          >
+            Github
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="nav-bar-item"
+            href="https://alumni.turing.io/alumni/dan-reardon"
+          >
+            Alumni Profile
+          </a>
         </nav>
         <div className="contact-container">
           <p className="contact-text">Email: dreardon1021@gmail.com</p>
@@ -22,7 +49,7 @@ const Footer = () => {
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
